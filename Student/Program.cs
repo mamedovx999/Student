c62ff6adb6cb76b5fe890c5ac7ab42ac0d6a0eda
mamedovx999 <mamedovx999@code.edu.az>
2024-03-19 18:37:36 +0400

    Add project files.

---
+++ Student/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Student
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Student students1 = new Student("Memmedeli", "Memmedov",20, 90);
+            Student students2 = new Student("Akif", "Niyazov", 21, 87);
+            Student student3 = new Student("Murad", "Tagiyev", 19, 75);
+
+            students1.ShowInfo();
+            Console.WriteLine("------------------------------------");
+            students2.ShowInfo();
+            Console.WriteLine("------------------------------------");
+            student3.ShowInfo();
+          
+
+
+        }
+    }
+}
---
+++ Student/Student.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Student
+{
+    internal class Student : IComparable<Student>
+    {
+        static int _id;
+        public int Id { get; }
+        public string? Name { get; set; }
+        public string? Surname { get; set; }
+        public int Age { get; set; }
+        public double Point { get; set; }
+
+        public static bool operator >(Student student1, Student student2)
+        {
+            return student1.Point > student2.Point;
+        }
+
+        public static bool operator <(Student student1, Student student2)
+        {
+            return student1.Point < student2.Point;
+        }
+
+        public void  ShowInfo()
+        {
+              Console.WriteLine($"Id: {Id}, Name: {Name}, Surname: {Surname}, Age: {Age}, Point: {Point}");
+            
+        }
+        public Student(string name, string surname ,int age ,double point)
+        {
+            _id++;
+            Id = _id;
+            Name = name;
+            Surname = surname;
+            Age = age;
+            Point = point;
+        }
+        public override string ToString() => $"{Id} Student : {Name} {Surname} // Point: {Point}";
+
+        public int CompareTo(Student? other)
+        {
+            return Point.CompareTo(other.Point);
+        }
+    }
+}
